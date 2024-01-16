b8cd59a6df932be50e36de982c1b4d5c2bd16862
ChuckWeindorf <charles.weindorf@mudsox.com>
2024-01-16 14:13:24 -0500

    First try exercize

--- app.js
+++ app.js
@@ -4,7 +4,35 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
+console.log(lastName, age);
 
 // Exercise 2
+const firstName = "Charles";
+lastName = "Weindorf";
+age = 62;
+console.log(firstName, lastName, age);
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(language, createdYear, isCaseSensitive);
+
+// Exersize 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(price, isOnSale, salePercentage, stock, inStock, selectedSize);
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(title, author, pageCount, bookmark, hasRead);
